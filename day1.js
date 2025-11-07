30be5689e05bf893e64505980850156105487a17
Mayank <mk20040307@gmail.com>
2025-11-07 21:13:50 +0530

    First day of JavaScript

---
+++ day1.js
@@ -0,0 +1,15 @@
+// const,let,var
+
+/*
+  let name="Mayank"
+  let rollNo=40
+  console.table([name,rollNo])  //  this print values in tabular form
+*/
+
+// tc39 and mdn are imp documentation
+// bigint for storing large value
+// let name; => undefined        means initialize but not defined
+// null => not 0 but empty     but object 
+
+// symbol is a datatype for UNIQUENESS
+
