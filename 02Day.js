cce8dacf72ddfdefbbc015e28fb41750679275bb
Mayank <mk20040307@gmail.com>
2025-11-08 14:47:25 +0530

    conversions ans important opr\erators

---
+++ 02Day.js
@@ -0,0 +1,31 @@
+/*
+console.log(typeof val)
+console.log(typeof(val))
+
+conversion
+       FOR INT
+let val="22"
+let valInNumber=Number(val)
+         IMP
+ if val="33abc"
+ let valInNumber=Number(val)
+ console.log(valInNumber)    => return NaN
+ if val=NULL   => return 0 after conversion
+ if val==undefined   => return NaN not a number
+ 
+           FOR BOOL
+ let isTrue=1;
+ let covert = Boolean(isTrue);    =. return true
+ "" => false
+ "mak" => true
+
+        FOT STRING
+33  => 33 for String 33
+
+         OPERATION
+console.log(1+2+"2")  return 32
+console.log("1"+2+2)  return 122
+console.log(+true) return 1      
+console.log(true+) return error      
+console.log(+"") return 0 coz "" is false      
+*/ 
