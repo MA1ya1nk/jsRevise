83ba6a279799567542c94820f6d0179c2651add6
Mayank <mk20040307@gmail.com>
2025-11-14 15:53:25 +0530

    string functions, math and numbers

---
+++ 04Day.js
@@ -0,0 +1,38 @@
+/*
+const name = "Mayank"
+const repoCount = 50
+console.log("Hello, my name is " + name + ". I have " + repoCount + " repositories on GitHub.")  this is old way
+console.log(`Hello, my name is ${name}. I have ${repoCount} repositories on GitHub.`)   this is latest and recommended way 
+
+const gameName = new String('BGMI')    another way of initializing string
+
+all string functions of js takes stack memory sso does not change actual string
+name.indexOf('y') return index of y
+
+.trim()   trim all extra spaces
+url.replace('%','-')   replace al chars of string from % to -
+.split('-') convert string into array base on - mak-am-a  into [mak,am,a]
+
+
+          NUMBER
+const bal=100
+console.log(bal.toFixed(2))   it give 100.00 decimal
+console.log(Math.round(num))  if num=23.4 return 23  if 23.5111 return 24
+.Precision(3)     
+
+num,MIN_VALUE,MAX_VALUE give 
+
+Math.abs(-4) return 4 
+Math.ceil(4.2) return 5 upper val even for 4.99
+Math.floor(4.1) return 4return lower value
+
+
+Math.random() return random value b/w 0 to 1 like 0.11111 andd 0 and 1 even
+Math.random()*10+1 return b/w 1 to 10.99999
+Math.floor(Math.random()*10) return int
+   
+IMP for val b/w 10 and 20
+const min=10
+const max=20
+Math.floor(Math.random() * (max-min+1) + min)
+*/ 
